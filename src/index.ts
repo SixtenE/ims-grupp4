e627e7e3fc1bf6cb675b7c4319b74b9f95f98a61
SixtenE <sixten.ekblad@gmail.com>
2025-09-13 17:13:58 +0200

    set up routes and controllers

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express, { Response, Request, Application } from "express";
 import "dotenv/config";
+import productRouter from "./routes/productRouter";
+import manufacturerRouter from "./routes/manufacturerRouter";
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 
@@ -9,6 +11,9 @@ app.use(express.json());
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({ message: "mongokjell" });
 });
+
+app.use("/api", productRouter);
+app.use("/api", manufacturerRouter);
 
 const server = app.listen(PORT, "::", () => {
   console.log(`Server is running at ${JSON.stringify(server.address())}`);
